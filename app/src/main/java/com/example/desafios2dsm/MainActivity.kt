7a9a2e118b6501f23d658fd496df5fce59c6e501
MariaTorres0 <mariatorres11001@gmail.com>
2025-03-27 00:07:07 -0600

    Diseño inicial de pantallas

---
+++ app/src/main/java/com/example/desafios2dsm/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.desafios2dsm
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val btnRegister = findViewById<Button>(R.id.btnRegister)
+
+        btnLogin.setOnClickListener {
+            startActivity(Intent(this, InicioSesionActivity::class.java))
+        }
+
+        btnRegister.setOnClickListener {
+            startActivity(Intent(this, RegistroActivity::class.java))
+        }
+    }
+}
