b745549102287c038c0c05d7e693053864ba8bfb
MariaTorres0 <mariatorres11001@gmail.com>
2025-03-28 09:43:12 -0600

    Diseño de pantallas faltantes

---
+++ app/src/main/java/com/example/desafios2dsm/ActualizarActivity.kt
@@ -0,0 +1,14 @@
+package com.example.desafios2dsm
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ActualizarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actualizar)
+    }
+}
